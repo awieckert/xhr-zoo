ebb3ccf1f8df848b6142fbc38230cc86d2f65274
Adam Wieckert <wiec1369@gmail.com>
2018-03-27 19:33:35 -0500

    Added the background colors to carnivores and veggies on button click

--- main.js
+++ main.js
@@ -15,16 +15,51 @@ const domString = (animalArray) => {
         stringToPrint +=   `<h3>${item.number}</h3>`;
         stringToPrint +=   `<img src="${item.imageUrl}" alt="RAWR!">`;
         stringToPrint +=   `<div class="button-container">`;
-        stringToPrint +=     `<button type="submit">Escaped</button>`;
+        stringToPrint +=     `<button class="escaped" type="submit">Escaped</button>`;
         stringToPrint +=   `</div>`;
         stringToPrint +=  `</div>`;
     });
     printToDom(stringToPrint, "zoo");
 }
 
+
+const addEscapedEvenListeners = () => {
+    const escapedButtons = document.getElementsByClassName("escaped");
+    
+    for(let i = 0; i < escapedButtons.length; i++){
+        escapedButtons[i].addEventListener("click", animalEscaped);
+    }
+}
+
+const animalEscaped = () => {
+
+    showCarnivores();
+    showVegetables();
+}
+
+const showCarnivores = () => {
+    const carnivores = document.getElementsByClassName('carnivore');
+    for(let i = 0; i < carnivores.length; i++){
+        carnivores[i].children[3].innerHTML = "";
+        carnivores[i].classList.add('red');
+    }
+}
+
+const showVegetables = () => {
+    const veggies = document.getElementsByClassName('vegetable');
+    for(let i = 0; i < veggies.length; i++){
+        veggies[i].children[3].innerHTML = "";
+        veggies[i].classList.add('green');
+    }
+}
+
+
+
+
 function onLoad () {
     const data = JSON.parse(this.responseText);
     domString(data.animals);
+    addEscapedEvenListeners();
 }
 
 function onFail () {
